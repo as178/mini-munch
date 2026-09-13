6bbf50f2da0e87d80e72bdc6b00a5a1f3ec51add
as178 <shadowangel753@gmail.com>
2026-09-13 17:59:49 +1200

    modifications in order model to support single order per table logic, and additional exports for future use

--- mini-munch-backend/models/Order.ts
+++ mini-munch-backend/models/Order.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Types } from "mongoose";
+import { Schema, model, Types, type HydratedDocument } from "mongoose";
 
 // defined type for the status of an order
 export const ORDER_STATUSES = [
@@ -10,7 +10,7 @@ export const ORDER_STATUSES = [
 export type OrderStatus = (typeof ORDER_STATUSES)[number];
 
 // interface to define the structure of an order item embedded document
-interface OrderItem {
+export interface OrderItem {
   menuItem: Types.ObjectId; // reference to the MenuItem document in MongoDB
   name: string;
   price: number;
@@ -18,13 +18,16 @@ interface OrderItem {
 }
 
 // interface to define the structure of an order document in MongoDB
-interface Order {
+export interface Order {
   table: Types.ObjectId; // reference to the Table document in MongoDB
   items: OrderItem[];
   total: number;
   status: OrderStatus;
   createdAt: Date;
 }
+
+// defined type for an order document in MongoDB
+export type OrderDocument = HydratedDocument<Order>;
 
 // defined schema for an order item embedded document
 const orderItemSchema = new Schema<OrderItem>(
@@ -69,16 +72,13 @@ const orderSchema = new Schema<Order>({
     type: Schema.Types.ObjectId, // reference to the Table document in MongoDB
     ref: "Table",
     required: [true, "Table is required."],
+    unique: true, // ensures that only one order can be created for a table at a time
   },
 
   // items is a list of all order items
   items: {
     type: [orderItemSchema],
     required: [true, "Order items are required."],
-    validate: {
-      validator: (items: OrderItem[]) => items.length > 0,
-      message: "Order must contain at least one item.",
-    },
   },
 
   // total is a number value that represents the total price of the order
