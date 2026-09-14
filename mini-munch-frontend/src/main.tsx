f4458852e60e58450748a370a41b632347aa563f
as178 <shadowangel753@gmail.com>
2026-09-14 16:16:19 +1200

    installed tailwindcss to aid with basic styling of the frontend

--- mini-munch-frontend/src/main.tsx
+++ mini-munch-frontend/src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./router.tsx";
 import { Toaster } from "react-hot-toast";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
