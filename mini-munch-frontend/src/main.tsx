62ec7139663a96303b9056ee9cbcf4a99b4e8cf3
as178 <shadowangel753@gmail.com>
2026-09-14 15:52:29 +1200

    implemented toasts to enable error pop-ups within the frontend

--- mini-munch-frontend/src/main.tsx
+++ mini-munch-frontend/src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./router.tsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* router for pages */}
     <RouterProvider router={router} />
+
+    {/* toaster for all notifications */}
+    <Toaster />
   </StrictMode>,
 );
--- mini-munch-frontend/src/pages/TableInput.tsx
+++ mini-munch-frontend/src/pages/TableInput.tsx
@@ -1,6 +1,7 @@
 import type { JSX } from "react";
 import { api } from "../services/api";
 import { AxiosError } from "axios";
+import toast from "react-hot-toast";
 
 export default function TableInput(): JSX.Element {
   async function testTableRequest(): Promise<void> {
@@ -8,9 +9,16 @@ export default function TableInput(): JSX.Element {
       const response = await api.get("/tables/17");
 
       console.log("Table response:", response.data);
+      toast.success(
+        `Table ${response.data.table.tableNumber} is available: ${response.data.table.available}`,
+      );
     } catch (error: unknown) {
       if (error instanceof AxiosError) {
         console.error("Table request failed:", error.response?.data);
+        toast.error(
+          `${error.response?.data.code}
+          ${error.response?.data.message}`,
+        );
       }
     }
   }
